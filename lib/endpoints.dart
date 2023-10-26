cc87d97bbdd160f8a82ab6b1d163167eb18c58a1
tomDev <34895823+thomasfranke@users.noreply.github.com>
2023-10-26 17:00:49 -0300

    update

--- lib/endpoints.dart
+++ lib/endpoints.dart
@@ -1,9 +1,9 @@
 import 'package:api/exports.dart';
 
 class MUploaderEndpoints {
-  static ApiModelEndpoints delete = ApiModelEndpoints(method: "POST", url: "m/filemanager/v1/delete");
-  static ApiModelEndpoints upload = ApiModelEndpoints(method: "POST", url: "m/filemanager/v1/upload");
-  static ApiModelEndpoints uploadRegularBytes = ApiModelEndpoints(method: "POST", url: "m/filemanager/v1/upload/bytes");
-  static ApiModelEndpoints uploadChunk = ApiModelEndpoints(method: "POST", url: "m/filemanager/v1/upload/chunk");
-  static ApiModelEndpoints uploadChunkBytes = ApiModelEndpoints(method: "POST", url: "m/filemanager/v1/upload/chunk/bytes");
+  static ApiModelEndpoints delete = ApiModelEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/delete");
+  static ApiModelEndpoints upload = ApiModelEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/upload");
+  static ApiModelEndpoints uploadRegularBytes = ApiModelEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/upload/bytes");
+  static ApiModelEndpoints uploadChunk = ApiModelEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/upload/chunk");
+  static ApiModelEndpoints uploadChunkBytes = ApiModelEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/upload/chunk/bytes");
 }
