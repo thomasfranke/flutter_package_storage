ac3fc6d88d85b33def7c7dac2b78d63f54be4751
tomDev <34895823+thomasfranke@users.noreply.github.com>
2023-12-09 17:39:25 -0300

    update

--- lib/endpoints.dart
+++ lib/endpoints.dart
@@ -1,9 +1,9 @@
 import 'exports.dart';
 
 class StorageEndpoints {
-  static ApiModelsEndpoints delete = ApiModelsEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/delete");
+  static ApiModelsEndpoints delete = ApiModelsEndpoints(method: ApiMethods.post, url: "/storage/v1/delete");
   static ApiModelsEndpoints upload = ApiModelsEndpoints(method: ApiMethods.post, url: "/storage/v1/upload/full/files");
-  static ApiModelsEndpoints uploadRegularBytes = ApiModelsEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/upload/bytes");
-  static ApiModelsEndpoints uploadChunk = ApiModelsEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/upload/chunk");
-  static ApiModelsEndpoints uploadChunkBytes = ApiModelsEndpoints(method: ApiMethods.post, url: "m/filemanager/v1/upload/chunk/bytes");
+  static ApiModelsEndpoints uploadBytes = ApiModelsEndpoints(method: ApiMethods.post, url: "/storage/v1/upload/full/bytes");
+  static ApiModelsEndpoints uploadChunk = ApiModelsEndpoints(method: ApiMethods.post, url: "/storage/v1/upload/chunk");
+  static ApiModelsEndpoints uploadChunkBytes = ApiModelsEndpoints(method: ApiMethods.post, url: "/storage/v1/upload/chunk/bytes");
 }
